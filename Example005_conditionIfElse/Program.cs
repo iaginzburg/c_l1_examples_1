4b491f5a31ef9c70beb6b14836dad0290ce5675d
Ilya Ginzburg <iaginzburg88@gmail.com>
2022-06-21 21:17:24 +0300

    add example005_conditionIfElse

---
+++ Example005_conditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введитте имя пользователя");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "илья")
+{
+Console.WriteLine("Привет, админ");
+}
+else
+{
+Console.Write("Привет, пользователь ");
+Console.WriteLine(userName);
+}
