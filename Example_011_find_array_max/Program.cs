81fffe9bcf7b05d69ab95b31674e7acebd5cd684
Ilya Ginzburg <iaginzburg88@gmail.com>
2022-06-25 14:25:51 +0300

    add Example012 (task 4 from lection 2) and update Example011

--- Example_011_find_array_max/Program.cs
+++ Example_011_find_array_max/Program.cs
@@ -1,4 +1,4 @@
-﻿int[] array = {12, 0, 134, -10, -233, 12, 2, 201, 33};
+﻿int[] array = { 12, 0, 134, -10, -233, 12, 2, 201, 33 };
 
 Console.WriteLine("введите значение элемента массива индекс которого необходимо найти");
 int find = int.Parse(Console.ReadLine());
@@ -9,11 +9,13 @@ int find = int.Parse(Console.ReadLine());
 int index = 0;
 // int find = -233;
 
-while (index < array.Length) {
-if (array[index] ==  find) {
-    Console.WriteLine("индекс искомого элемента массива " + index);// 
-    //break;
-}
-index++;
+while (index < array.Length)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine("индекс искомого элемента массива " + index);// 
+                                                                       //break;
+    }
+    index++;
 
 }
---
+++ Example_012_findArrayMaxWithMethods/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+    Console.WriteLine();
+}
+
+int FindIndexOfElement(int[] collect, int find)
+{
+    int index = 0;
+    while (index < collect.Length)
+    {
+        if (collect[index] == find)
+        {
+            Console.WriteLine("индекс искомого элемента массива " + index);
+            break;
+        }
+        index++;
+    }
+    if (index == collect.Length)
+    {
+        Console.WriteLine("в массиве отсутствует элемент с заданным значением ");
+        return -1;
+    }
+;
+    return index;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+// FindIndexOfElement(array, 37);
+Console.WriteLine(FindIndexOfElement(array, 37));
