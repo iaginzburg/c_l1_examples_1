1bf5233dcd36e7dbc2a7105bef41b05cebeba8af
Ilya Ginzburg <iaginzburg88@gmail.com>
2022-06-25 13:22:18 +0300

    add Example 011 (lection2, 3rd)

---
+++ Example_011_find_array_max/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = {12, 0, 134, -10, -233, 12, 2, 201, 33};
+
+Console.WriteLine("введите значение элемента массива индекс которого необходимо найти");
+int find = int.Parse(Console.ReadLine());
+
+// int lenght = array.Length;
+// Console.WriteLine(lenght);
+
+int index = 0;
+// int find = -233;
+
+while (index < array.Length) {
+if (array[index] ==  find) {
+    Console.WriteLine("индекс искомого элемента массива " + index);// 
+    //break;
+}
+index++;
+
+}
