6679f14c387378b25dffc02a5b58b981b444cc31
Ilya Ginzburg <iaginzburg88@gmail.com>
2022-07-09 14:05:50 +0300

    added examples from lection4

---
+++ Example_018_factorial/Program.cs
@@ -0,0 +1,13 @@
+﻿double Factorial (int num){
+    if (num == 1) return 1;
+    return num*Factorial(num-1);
+}
+// Console.WriteLine(Factorial(1));
+// Console.WriteLine(Factorial(2));
+// Console.WriteLine(Factorial(3));
+// Console.WriteLine(Factorial(4));
+
+for (int i = 1; i < 40; i++){
+
+   Console.WriteLine($"{i}! = {Factorial(i)}");
+}
---
+++ Example_18_Fibonahcci/Program.cs
@@ -0,0 +1,9 @@
+﻿int Fibonacci (int n){
+if (n == 1) return 1;
+if (n == 2) return 1;
+return Fibonacci(n-1) + Fibonacci (n-2);
+}
+
+for (int i = 1; i < 10; i++){
+Console.WriteLine($"{i} - {Fibonacci(i)}");
+}
