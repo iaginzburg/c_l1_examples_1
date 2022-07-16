e19841b1e7d32058b042a28615612c83cf775278
Ilya Ginzburg <iaginzburg88@gmail.com>
2022-07-16 18:10:34 +0300

    added Example 019 about friendly code writing (parsing of string

---
+++ Example_019/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (6,7)"
+                    .Replace("(", "")
+                    .Replace(")", "");// "1,2 2,3 4,5 6,7"
+
+// string[] data = text.Split(" ");
+//                  .ToArray();
+
+var data = text.Split(" ") //разбили на элементы по разделителю пробел
+                .Select(e => e.Split(','))//разделили элементы по разделителю запятая
+                .Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1])))//сделали из массива массивов - массив кортежей (сразу конвертировали в число), дали наименования элементам кортежа  
+                .Where (e => (e.x%2 == 0))
+                .Select (point => (point.x*10, point.y*10))
+                .ToArray();
+for (int i = 0; i < data.Length; i++){
+    Console.WriteLine(data[i]);
+    Console.WriteLine();
+}
+
+ 
